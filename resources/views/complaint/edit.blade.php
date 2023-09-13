88e5807d9fa96cc6f562577cc5831539889f400b
user003 <sheila.muzni@perak.gov.my>
2023-09-13 10:33:08 +0800

    Add complaint, action and action status

---
+++ resources/views/complaint/edit.blade.php
@@ -0,0 +1,24 @@
+@extends('layouts.app')
+
+@section('content')
+{{-- //HI FROM EDIT --}}
+<div class ="container">
+    <div class ="row">
+        <div class ="col-md 12">Testing</div>
+    </div>
+</div>
+@endsection
+
+@push('style')
+<style>
+    body{
+        background-color: lightblue;
+    }
+</style>
+@endpush
+
+{{-- @push('script')
+<script>
+    alert('welcome edit page');
+</script> 
+@endpush --}}
